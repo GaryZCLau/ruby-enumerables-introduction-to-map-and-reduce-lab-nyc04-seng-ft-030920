f28b39f03eb2fd71c62f2240ba93655c89355c9e
benign-fork-5056 <gareeeliu@gmail.com>
2020-03-04 22:34:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,10 +57,10 @@ def reduce_to_total(source_array, starting_point = "0".to_i)
 end
 
 def reduce_to_all_true(source_array)
-  i = 0
-  while i < source_array.length
-
-  end
+  # i = 0
+  # while i < source_array.length
+  #
+  # end
 end
 
 def reduce_to_any_true(source_array)
